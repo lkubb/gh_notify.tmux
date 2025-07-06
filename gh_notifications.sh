3830aebe9366c9f75e2882f7f55158945c9b0c31
jeanluc <lkubb@protonmail.com>
2025-07-06 15:32:07 +0200

    Implement cache to avoid API limit w/ low status-interval

--- gh_notifications.sh
+++ gh_notifications.sh
@@ -2,13 +2,33 @@
 
 set -e
 
-if command -v jq &> /dev/null; then
-  notifications=$(gh api notifications | jq length)
+cache_file="${XDG_CACHE_HOME:-$HOME/.cache}/tmux_gh_notifications"
+refresh_interval="$(tmux show-option -gqv '@gh_notifications_refresh_interval')"
+refresh_interval="${refresh_interval:-60}"
+
+# BSD stat uses a different interface than the GNU coreutils one
+if [[ "$OSTYPE" != darwin* ]]; then
+  stat_cmd="stat -c %Y"
+# Homebrew installs GNU coreutils with g prefix. Users might have linked those without prefix,
+# so explicitly try to use them to avoid erroring in the latter case.
+elif command -v gstat &>/dev/null; then
+  stat_cmd="gstat -c %Y"
 else
-  notifications=$(gh api notifications | grep -o 'unread' | wc -l)
+  stat_cmd="stat -f %m"
 fi
 
-if (( notifications > 0 )); then
+if [[ -f "$cache_file" ]] && [[ $(($(date +%s) - $($stat_cmd "$cache_file"))) -lt "$refresh_interval" ]]; then
+  notifications=$(<"$cache_file")
+else
+  if command -v jq &>/dev/null; then
+    notifications=$(gh api notifications | jq length)
+  else
+    notifications=$(gh api notifications | grep -o 'unread' | wc -l)
+  fi
+  echo "$notifications" >"$cache_file"
+fi
+
+if ((notifications > 0)); then
   echo -n '#[fg=colour7]#[default]'
   echo -n "#[bg=colour7,fg=colour0,bold] $notifications#[default]"
   echo -n '#[fg=colour7]#[default]'
